6a25444afc3b0ea9e44d9a8d2afa9f52b6b3a8d0
fedorenk <eugenedrm@gmail.com>
2023-12-28 17:12:33 +0000

    Initial commit

---
+++ src/main/java/eugenedrm/home/educative/hexagonalarchitecture/HexagonalArchitectureWebAppEducativeApplication.java
@@ -0,0 +1,13 @@
+package eugenedrm.home.educative.hexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureWebAppEducativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureWebAppEducativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eugenedrm/home/educative/hexagonalarchitecture/HexagonalArchitectureWebAppEducativeApplicationTests.java
@@ -0,0 +1,13 @@
+package eugenedrm.home.educative.hexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureWebAppEducativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
